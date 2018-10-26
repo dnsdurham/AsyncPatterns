2340d15d99f237be68e2f0e462f28ade383715f8
Doug Durham <ddurham@dontpaniclabs.com>
2018-10-26 12:56:35 -0500

    Added solution and client projects

---
+++ AsyncPatterns.AsyncAwait.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AsyncPatterns.AsyncAwait.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ AsyncPatterns.Tasks.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AsyncPatterns.Tasks.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
